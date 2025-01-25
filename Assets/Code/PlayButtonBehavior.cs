b0f71afa100fafa660a342872cf73167152549aa
GM <gianmad@estudiantes.uci.cu>
2025-01-25 18:07:48 -0500

    Added MenuScene

---
+++ Assets/Code/PlayButtonBehavior.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonBehavior : MonoBehaviour
+{
+    public void OnClick() {
+        print("Play from button");
+    }
+}
